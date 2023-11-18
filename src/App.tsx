b98c19e2cf896315cf5f36b7419b3f2733a9513b
rz-regina <regina.sherlin@roanuz.com>
2023-11-18 20:23:31 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import WackAMole from "./comp/WackAMole";
+
+function App() {
+  return (
+    <div className='App'>
+      <WackAMole />
+    </div>
+  );
+}
+
+export default App;
